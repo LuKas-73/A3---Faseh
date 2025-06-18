dbe732ff7bd042944cfbf1db0116a4e39e2917b9
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 21:11:10 -0300

    Create ConexaoComBancoDeDados.java

---
+++ src/main/java/com/seuusuario/agendamento/ConexaoComBancoDeDados.java
@@ -0,0 +1,15 @@
+package com.seuusuario.agendamento;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoComBancoDeDados {
+    private static final String URL = "jdbc:mysql://localhost:3306/Agendamento";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "lukas73";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+}
